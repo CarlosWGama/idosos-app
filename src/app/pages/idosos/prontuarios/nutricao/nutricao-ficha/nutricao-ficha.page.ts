6a8e4480af7099e145d71db5d8ee1a14a080c9c8
Carlos W. Gama <carloswgama@gmail.com>
2020-09-11 12:02:11 -0300

    Ajuste Fisioterapeuta

--- src/app/pages/idosos/prontuarios/nutricao/nutricao-ficha/nutricao-ficha.page.ts
+++ src/app/pages/idosos/prontuarios/nutricao/nutricao-ficha/nutricao-ficha.page.ts
@@ -14,79 +14,82 @@ export class NutricaoFichaPage extends FichaAvaliacaoModelo implements OnInit {
   mediaMaoEsquerda = '0';
 
   async ngOnInit() {
+   
     this.form = this.formBuilder.group({
       'id': null,
       'data': [moment().format('YYYY-MM-DD'), Validators.required],
-
-      'diagnostico_clinico': [null],
-      'queixas_principais': [null],
-
-      //Mini Mental Test
-      'clock_task': [null],
-      'barthel': [null],
-      'ppt': [null],
-      
-      //Sinais vitais
-      'fc': [null],
-      'fr': [null],
-      't': [null],
-      'pa': [null],
-      
-      //Nível de Consciência
-      'nivel_consciencia': [1],
-      
-      //Estado Mental
-      'estado_mental': [1],
-      
-      //Sistema Respiratório
-      'sistema_respiratorio': [1],
-      'ventilado_suporte_o2': [null],
-      'ritmo': [1],
-      'padrao_muscular_ventilatório': [1],
-      'expansibilidade_toracica': [1],
-      'expansibilidade_toracica_assimetrica': [null],
-      'ausculta': [1],
-      'ausculta_mv_diminuido': [null],
-      'ausculta_mv_abolido': [null],
-      'ruidos_adventicios': [1],
-      'tosse': [1],
-      'aspecto_secrecao': [null],
-      
-      //SISTEMA OSTEOMIOARTICULAR
-      'sistema_osteomioarticular': [1],
-      'forca_muscular': [1],
-      'tonus': [1],
-      'amplitude_articular_normal': [false],
-      'amplitude_articular_diminuida': [false],
-      'amplitude_articular_diminuida_obs': [null],
-      'amplitude_articular_luxacao': [false],
-      'amplitude_articular_luxacao_obs': [null],
-      'amplitude_articular_rigidez': [false],
-      'amplitude_articular_rigidez_obs': [null],
-      'amplitude_articular_fratura': [false],
-      'amplitude_articular_fratura_obs': [null],
-      'amplitude_articular_desvio': [false],
-      'amplitude_articular_desvio_obs': [null],
-      
-      //Deambutação
-      'deambutacao': [1],
-      'marcha': [null],
-      'equilibrio': [1],
-      'equilibrio_anormal': [null],
-      'pele': [null],
-      'edema_local': [null],
-      'edema_tipo': [null],
-      'edema_grau': [null],
-      'sequelas': [null],
-      'aparelho_genitourinario': [1],
-      'aparelho_genitourinario_incontigencia': [null],
-      'aparelho_genitourinario_funcao_sexual': [null],
-
-      //Final
+      'saude_gastrointestinal': [[]],
+      'habito_intestinal_dia':null,
+      'habito_intestinal_semana':null,
+      'consistencia_fezes':[0], 
+      'laxante': [false],
+      'apetite': [1],
+      'sobra_comida': [0],
+      'aversao_alimentar': null, 
+      'intolerencia_alimentar': null, 
+      'alergia_alimentar': null, 
+      'sede': [false],
+      'liquidos_diarios': [2],
+      'liquido_consumo': [1],
+      'suplemento': [false],
+      //Antroometria
+      'peso_atual': [null], 
+      'peso_usual': [null],
+      'peso_estimado': [null], 
+      'perda_peso': [null],
+      'membro_amputado': [[]],
+      'segmentacao_amputado':null, 
+      'altura':[null],
+      'altura_joelho': [null],
+      'semi_envergadura': [null],
+      'altura_estimada': [null], 
+      'imc': [null], 
+      'circunferencia_panturrilha': [null], 
+      'circunferencia_braco': [null],
+      'circunferencia_pulso': [null], 
+      'dct': [null],
+      'dcse': [null], 
+      'circunferencia_muscular_braco':[null],
+      'circunferencia_cintura':[null], 
+      'circunferencia_cintura_tipo':[1], 
+      'marcapasso': [false],
+      'edema':[false], 
+      'edema_localizacao':null,
+      'cacifo': [false],
+      'lado_dominante': [1],
+      //Avalição força palmar
+      'fp_mao_direita1': [0],
+      'fp_mao_direita2': [0],
+      'fp_mao_direita3': [0],
+      'fp_mao_esquerda1': [0],
+      'fp_mao_esquerda2': [0],
+      'fp_mao_esquerda3': [0],
+      //Exame física
+      'c_consumo_musculo_temporal': [false],
+      'c_consumo_bola_gordurosa': [false],
+      'c_arco_zigomatico_aparente': [false],
+      'c_depressao_masseter': [false],
+      't_clavicula_aparente':[false],
+      't_esterno_aparente':[false],
+      't_ombros_quadrados':[false],
+      'p_proeminência_supra_infra': [false],
+      'o_esclerotica': [false],
+      'o_mucosa_hipocoradas': [false],
+      'o_orbitas_profundas': [false],
+      'cf_coloracao_mucosa': [1],
+      'ms_edema': [false],
+      'mi_edema': [false],
+      'mi_joelho_quadrado':[false],
+      'pele_manchas':[false],
+      'pele_ressecamento':[false],
+      'pele_lesoes':[false],
+      'pele_turgor':[false],
+      'pele_prurido':[false],
       'diagnostico_fisioterapeutico': [null],
       'objetivos': [null],
       'conduta': [null]
     })
+
 
     await super.ngOnInit(); 
   }
