06be7ec922e7f3441a1100fa8181428eace0bbd8
Carlos W. Gama <carloswgama@gmail.com>
2020-09-14 13:17:49 -0300

    Ajuste do reactive form module

--- src/app/pages/idosos/prontuarios/fisioterapia/ficha/ficha.module.ts
+++ src/app/pages/idosos/prontuarios/fisioterapia/ficha/ficha.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FichaPageRoutingModule } from './ficha-routing.module';
-
 import { FichaPage } from './ficha.page';
 import { NgxMaskIonicModule } from 'ngx-mask-ionic';
 
 @NgModule({
   imports: [
+    NgxMaskIonicModule,
     CommonModule,
     FormsModule,
     IonicModule,
     ReactiveFormsModule,
-    NgxMaskIonicModule,
     FichaPageRoutingModule
   ],
   declarations: [FichaPage]
